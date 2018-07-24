cb0537be8d9f8f8d3ac3d18c080217c4797462da
Sandro Simas <sandro.csimas@gmail.com>
2018-07-24 10:58:49 -0300

    Release 0.0.2

--- tasks/publish.js
+++ tasks/publish.js
@@ -10,15 +10,10 @@ async function lintProject() {
   await commands.exec('npm run lint', WORKING_DIR);
 }
 
-async function buildProject() {
-  // Nothing to do...
-}
-
 // Run this if call directly from command line
 if (require.main === module) {
-  publish.withWorkingDir(WORKING_DIR);
-  publish.withLintTask(lintProject);
-  publish.withBuildTask(buildProject);
-  publish.isNpmProject(true);
+  publish.setWorkingDir(WORKING_DIR);
+  publish.setLintTask(lintProject);
+  publish.setNpmProject(true);
   publish.run();
 }
--- tasks/release.js
+++ tasks/release.js
@@ -10,14 +10,9 @@ async function lintProject() {
   await commands.exec('npm run lint', WORKING_DIR);
 }
 
-async function buildProject() {
-  // Nothing to do...
-}
-
 // Run this if call directly from command line
 if (require.main === module) {
-  release.withWorkingDir(WORKING_DIR);
-  release.withLintTask(lintProject);
-  release.withBuildTask(buildProject);
+  release.setWorkingDir(WORKING_DIR);
+  release.setLintTask(lintProject);
   release.run(process.argv[2], process.argv[3]);
 }
